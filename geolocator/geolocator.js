d0e45168c80f967caef1f4b988fc3bf0fc117092
Phantomical <phantomical@github.com>
2018-04-18 15:46:05 -0400

    Version number bump

--- geolocator/geolocator.js
+++ geolocator/geolocator.js
@@ -346,7 +346,7 @@
         id: "Geolocator",
         description: "An extension to tell you where you are.",
         author: "STEAMROLLER",
-        version: "0.1.1",
+        version: "0.1.2",
         settingsProvider: createSettingsProvider()
     });
 
